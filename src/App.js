df682d45f1e683caca53887eba2b6bb5de043eeb
Lammerlord92 <javcarfer92@gmail.com>
2017-07-18 14:46:49 +0200

    Cambios en el nombre de los archivos, mas lógicos

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react'
 import logo from './logo.svg';
 import './App.css';
-import ActualUnit from './ActualUnit.js';
+import ArmyList from './ArmyList.js';
 
 class App extends Component {
   render() {
@@ -12,8 +12,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React + MobX</h2>
         </div>
-
-        <ActualUnit/>
+        <div id="ProfileSheet" className="container">
+            <ArmyList/>
+        </div>
       </div>
     );
   }
